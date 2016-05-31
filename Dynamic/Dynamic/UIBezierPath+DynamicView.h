999442f56b04426a04a1def24a11999119c63ef4
zhangyi <zhangyi@zhangdeMac-mini.local>
2016-05-31 12:42:39 +0800

    half round

---
+++ Dynamic/Dynamic/UIBezierPath+DynamicView.h
@@ -0,0 +1,13 @@
+//
+//  UIBezierPath+DynamicView.h
+//  Dynamic
+//
+//  Created by zhangyi on 16/5/31.
+//  Copyright © 2016年 Hikvision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBezierPath (DynamicView)
+
+@end
---
+++ Dynamic/Dynamic/UIBezierPath+DynamicView.m
@@ -0,0 +1,13 @@
+//
+//  UIBezierPath+DynamicView.m
+//  Dynamic
+//
+//  Created by zhangyi on 16/5/31.
+//  Copyright © 2016年 Hikvision. All rights reserved.
+//
+
+#import "UIBezierPath+DynamicView.h"
+
+@implementation UIBezierPath (DynamicView)
+
+@end
